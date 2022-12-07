b66e822bd9100464fb7b20ee0bacfaa50838d738
Salome Charkviani <salo.charkviani@gmail.com>
2022-12-07 15:12:24 +0400

    install bootstrap and test

---
+++ src/app/game-controler/game-controler.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-controler',
+  templateUrl: './game-controler.component.html',
+  styleUrls: ['./game-controler.component.css']
+})
+export class GameControlerComponent {
+
+}
